299c18e06f97d3d55118f18e0438296f9d1ea150
varun <varunsiravuri@gmail.com>
2025-11-15 00:00:06 +0530

    clientquery

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver} from "@hookform/resolvers/zod";
 import TextareaAutosize from "react-textarea-autosize";
 import { ArrowUpIcon , Loader2Icon } from "lucide-react";
-import { useMutation, useQuery  } from "@tanstack/react-query";
+import { useMutation, useQuery , useQueryClient } from "@tanstack/react-query";
 interface Props{
     projectId: String;
 };
