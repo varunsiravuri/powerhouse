967e5cc439a151411b2d4168a9b365706e84a930
varun <varunsiravuri@gmail.com>
2025-11-14 23:58:29 +0530

    message-form

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -1,3 +1,10 @@
+import {z} from "zod";
+import { toast } from "sonner";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver} from "@hookform/resolvers/zod";
+import TextareaAutosize from "react-textarea-autosize";
+import { ArrowUpIcon , Loader2Icon } from "lucide-react";
 interface Props{
     projectId: String;
 };
