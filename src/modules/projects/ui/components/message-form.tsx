c594c0238ba3206bc9e7153aba21d1820852a933
varun <varunsiravuri@gmail.com>
2025-11-18 23:38:55 +0530

    import done

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -1,19 +1,21 @@
-import {z} from "zod";
+import { z } from "zod";
 import { toast } from "sonner";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { zodResolver} from "@hookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import TextareaAutosize from "react-textarea-autosize";
-import { ArrowUpIcon , Loader2Icon } from "lucide-react";
-import { useMutation, useQuery , useQueryClient } from "@tanstack/react-query";
-interface Props{
-    projectId: String;
-};
+import { ArrowUpIcon, Loader2Icon } from "lucide-react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+import { cn } from "@/lib/utils";
+import { useTRPC } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+import { Form, FormField } from "@/components/ui/form";
+
+interface Props {
+  projectId: String;
+}
 
 export const MessageForm = ({ projectId }: Props) => {
-    return (
-        <div>
-            Message Form 
-        </div>
-    );
+  return <div>Message Form</div>;
 };
