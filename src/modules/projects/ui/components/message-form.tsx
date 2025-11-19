416ce00cf905768338724424413a160761f92b35
varun <varunsiravuri@gmail.com>
2025-11-19 23:54:52 +0530

    stles to the chat

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -68,6 +68,17 @@ export const MessageForm = ({ projectId }: Props) => {
                 />
             )}
           />
+          <div className="flex gap-x-2 items-end justify-between pt-2">
+            <div className ="text-[10px] text-muted-foreground font-mono">
+                <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1
+                rounded border bg-muted px-1.5 font-mono text-[10px] font-medium
+                text-muted-foreground">
+                    <span>&#8984;
+                    </span>Enter
+                </kbd>
+            </div>
+
+          </div>
         </form>
       </Form>
     );
