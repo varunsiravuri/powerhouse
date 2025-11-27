1ceac384a4b907550fcc3b24a90706c22cb4ce09
varun <varunsiravuri@gmail.com>
2025-11-27 23:55:51 +0530

    hint added to the components

---
+++ src/components/hint.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "./ui/tooltip";
+
+
+interface HintProps{
+    children:React.Reactnode;
+    text: string;
+    side?: "top" | "right" | "bottom" | "left";
+    align?: "start" | "center" | "end";
+};
+
+export const Hint = ({
+    children,
+    text,
+    side = "top",
+    align = "center"
+}: HintProps) => {
+    return(
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    {children}
+                </TooltipTrigger>
+                <TooltipContent side ={side} align={align}>
+                    <p>{text}</p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    );
+};
