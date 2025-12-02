3c99bf93fdbdbd8e25a46ed0e735699043a25fc2
varun <varunsiravuri@gmail.com>
2025-12-02 23:45:29 +0530

    imports added

--- src/components/code-view/index.tsx
+++ src/components/code-view/index.tsx
@@ -14,12 +14,15 @@ interface Props{
 };
 
 export const CodeView = ({code,lang}:Props) => {
+    useEffect(()=>{
+        Prism.highlightAll();
+    },[code]);
     return(
         <pre className="p-2 bg-transparent border-none rounded-none m-0 text-xs">
             <code className={`language-${lang}`}>
                 {code}
             </code>    
         </pre>
-    )
+    );
 
-}
+};
---
+++ src/components/file-explorer.tsx
@@ -0,0 +1,12 @@
+import { CopyCheckIcon, CopyIcon } from "lucide-react";
+import {useState, useMemo, useCallback, Fragment} from "react";
+
+import { Hint } from "./hint";
+import { Button } from "./ui/button";
+import { CodeView } from "./code-view";
+import { ResizableHandle,ResizablePanelGroup,ResizablePanel } from "./ui/resizable";
+
+import { Breadcrumb, BreadcrumbEllipsis,BreadcrumbItem,BreadcrumbPage,BreadcrumbList,BreadcrumbSeparator } from "./ui/breadcrumb";
+
+
+
